97fd41ab7d7b97a306cb9474277f27db2e0d5026
lujiancheng <1036522761@qq.com>
2023-02-21 21:47:49 +0800

    修复地铁的参数

--- train_dynamic_model/AntiForce.m
+++ train_dynamic_model/AntiForce.m
@@ -2,13 +2,13 @@ function f = AntiForce(veo,pos)
 %UNTITLED6 计算附加阻力
 %单位是KN
 %    传入速度单位为m/s
-    M=428;
-   % u=veo;
+    M=194.295; 
     u=veo*3.6;
-    w0=0.66+0.00245*u+0.000132*u*u;
+    w0=2.031+0.0622*u+0.001807*u*u;
     wi=RoadGradinet(pos);
     %wr
     %ws
-    f=(w0+wi)*M*9.8/1000;%单位是KN
+    f=(w0+wi)*M*9.8/1000;
 end
+
 
--- train_dynamic_model/BrakeForce.m
+++ train_dynamic_model/BrakeForce.m
@@ -3,14 +3,10 @@ function f= BrakeForce(veo)
 %  传入速度单位为m/s
 %  阻力单位为KN
     f=0;
-    %u=veo;
     u=veo*3.6; %单位换算
-    if u<5
-        f=59.8*u;
-    elseif u<106.7
-        f=300-0.2851*u; 
-    elseif u<400
-         f=28880*3.6/u;
+    if u<77
+        f=166 ;
+    elseif u<80
+        f=0.1314*u*u-25.07*u+1300;  
     end
 end
-
